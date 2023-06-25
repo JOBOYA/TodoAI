bc696bc837f3c973d58899f0f92984cb875a6172
JOBOYA <lena.boya26@gmail.com>
2023-06-25 15:14:08 +0200

    gpt3.5

--- src/components/SummarizeButton.tsx
+++ src/components/SummarizeButton.tsx
@@ -1,23 +1,28 @@
 import React, { useState } from 'react';
 import { Button, Textarea } from "@chakra-ui/react";
 import { summarizeTasks } from './summarizeTasks';
+import useTaskCollection from '../hooks/useTaskCollection';
 
 const SummarizeButton = () => {
-    const tasks: string[] = ['Tâche 1', 'Tâche 2', 'Tâche 3'];
+    // Récupérer le tableau de tâches depuis le contexte
+    const [taskCollection] = useTaskCollection();
+
+    // Combiner toutes les tâches de différentes colonnes en un seul tableau
+    const tasks = Object.values(taskCollection).flat();
+
     const [summary, setSummary] = useState('');
 
     const handleClick = async () => {
-        const result = await summarizeTasks(tasks);
+        const result = await summarizeTasks(taskCollection, tasks.map(task => task.title));
         setSummary(result);
     };
 
     return (
         <>
-        <Button onClick={handleClick}>Résumer les tâches</Button>
-        <Textarea value={summary} placeholder="Le résumé apparaîtra ici..." />
+            <Button onClick={handleClick}>Résumer les tâches</Button>
+            <Textarea value={summary} placeholder="Le résumé apparaîtra ici..." />
         </>
     );
-
 };
 
 export default SummarizeButton;
--- src/components/summarizeTasks.tsx
+++ src/components/summarizeTasks.tsx
@@ -1,18 +1,26 @@
-// summarizeTasks.js
 import axios from 'axios';
 
-
-
 const OPENAI_API_URL = 'https://api.openai.com/v1/chat/completions';
-const API_KEY =  'YOUR_API_KEY_HERE'
+const API_KEY = process.env.VITE_OPENAI_API_KEY;
 
-export async function summarizeTasks(tasks: string[]) {
+interface Task {
+    title: string;
+}
+
+interface TaskCollection {
+    [key: string]: Task[];
+}
+
+export async function summarizeTasks(taskCollection: TaskCollection, tasks: string[]): Promise<string> {
     try {
+        const inProgressTasks = taskCollection['In Progress'].map((task: Task) => task.title).join(', ');
+        const blockedTasks = taskCollection['Blocked'].map((task: Task) => task.title).join(', ');
+        const completedTasks = taskCollection['Completed'].map((task: Task) => task.title).join(', ');
         const response = await axios.post(OPENAI_API_URL, {
             model: 'gpt-3.5-turbo',
             messages: [
                 { role: 'system', content: 'You are a helpful assistant.' },
-                { role: 'user', content: `Résumez les tâches suivantes: ${tasks.join(', ')}` }
+                { role: 'user', content: `Fournissez un état des tâches. En cours: ${inProgressTasks}. Bloquées: ${blockedTasks}. Terminées: ${completedTasks}.` }
             ]
         }, {
             headers: {
@@ -21,9 +29,9 @@ export async function summarizeTasks(tasks: string[]) {
             }
         });
 
-        console.log(response.data.choices[0].message.content);
+        return response.data.choices[0].message.content;
     } catch (error) {
         console.error('Error:', error);
+        return 'Une erreur est survenue lors de la récupération du résumé.';
     }
 }
-
